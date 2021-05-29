0e8cb41c60c2c96209b099a05eaf7ec00de37915
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-29 18:47:54 +0100

    Actor DB context class is created.

---
+++ EFCore/ConsoleApp/Entities/ActorDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.Entities
+{
+    public class SomeDB : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connStr = Environment.GetEnvironmentVariable("postgresconnstr");
+            Console.WriteLine("Connection String: " + connStr);
+            optionsBuilder.UseNpgsql(connStr ?? string.Empty);
+
+        }
+    }
+}
