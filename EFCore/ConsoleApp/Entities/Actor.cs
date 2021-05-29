0e4a97c8d638d4f975b5c1adff55006d4bb921e0
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-29 17:15:35 +0100

    Actor entity class is created.

---
+++ EFCore/ConsoleApp/Entities/Actor.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool AcademyWinner { get; set; }
+    }
+}
